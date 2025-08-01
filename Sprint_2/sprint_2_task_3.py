18f4b751dd51bcef512f13568f1cd37a32323edd
v.molchanov <v.molchanov@dssl.ru>
2025-08-01 13:41:13 +0300

    fix review task_3

--- Sprint_2/sprint_2_task_3.py
+++ Sprint_2/sprint_2_task_3.py
@@ -6,8 +6,10 @@ class PointsForPlace:
     def get_points_for_place(place):
         if place > 100:
             print("Баллы начисляются только первым 100 участникам")
+            return 0
         if place < 1:
             print("Спортсмен не может занять нулевое или отрицательное место")
+            return 0
         return 101 - place
 
 
@@ -16,6 +18,7 @@ class PointsForMeters:
     def get_points_for_meters(meters):
         if meters < 0:
             print("Количество метров не может быть отрицательным")
+            return 0
         return meters * 0.5
 
 
